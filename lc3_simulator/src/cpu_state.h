660ddad955653e77d8a56645ee72ba43b989c845
Alex Zhiyuan Zhong <alexzz2@illinois.edu>
2026-02-10 22:48:40 -0600

    "day 1"

--- lc3_simulator/src/cpu_state.h
+++ lc3_simulator/src/cpu_state.h
@@ -2,8 +2,12 @@
 #include <array>
 #include <cstdint>
 #include <iostream>
-using namespace std;
 struct CPUState{
-    array<uint_16t,8> reg;
-    
-}
+    std::array<uint16_t,8> reg;     /* Initialize R0-R7*/
+    uint16_t pc;    /* Initialize Program Counter in CPU */
+    bool n,z,p;     /* Condition codes for n,z,p */
+    CPUState();     /* Constructor */
+    void reset();   /* Reset state of LC3 CPU*/
+    void update_flags(uint16_t result);
+    void print() const;
+};
